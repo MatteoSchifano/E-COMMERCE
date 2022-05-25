e817f8807eb590a27e2c8ce6138afbb9cfc033b8
Matteo Schifano <matvinsch@gmail.com>
2022-05-25 14:29:02 +0200

    ultime teo

--- liste.py
+++ liste.py
@@ -1,6 +1,6 @@
 
 prodotti = ['iphone 13', 'jbl go', 'mela', 'pera',  'tel samsung', 'palla', 'guanti' ]
-produttori = ['apple', 'cassa', 'melinda', 'perinda',  'samsung', 'bho', 'adidas' ]
+produttori = ['apple', 'cassa', 'melinda', 'perinda',  'samsung', 'apple', 'adidas' ]
 prezzi = [1000,50,2,3,600,10,15]
 
 tags = [('telefono', 'apple', 'iphone', 'informatica'), ('cassa', 'informatica', 'musica'),
