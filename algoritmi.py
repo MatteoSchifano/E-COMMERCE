0c0e190c74d9977c08cfc762b9d3d5495ca3f8e5
Matteo Schifano <matvinsch@gmail.com>
2022-06-02 15:48:00 +0200

    fine

--- algoritmi.py
+++ algoritmi.py
@@ -1,22 +1,21 @@
 # ----------------------------------------------------------------
 # liear regression
 # ----------------------------------------------------------------
-from liste import prodotti,tags,prezzi,produttori
+from liste import prodotti, tags, prezzi, produttori
 from back import Extract, GestisciProdotto
 
+from bson.objectid import ObjectId
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.neighbors import NearestNeighbors
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+
 from sklearn.preprocessing import OrdinalEncoder
 
+
 def modella_csv():
-    df = pd.read_csv('Product.csv') 
+    df = pd.read_csv('Product.csv')
 
     # utilizziamo le colonne strettamente necessarie
     nomi = df.iloc[:, 1]
@@ -44,63 +43,70 @@ def modella_csv():
 
 
 def preprocessing(df):
-    # permette di la divisione dei tags 
-    df.tags=df.tags.str[1:-1].str.split(',').tolist()
-    
+    # permette di la divisione dei tags
+    df.tags = df.tags.str[1:-1].str.split(',').tolist()
+
     oe = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
     # lista di tag totali
     tag_tot = [t for x in tags for t in x]
-    # ottengo ordinal encoder della lista     
+    # ottengo ordinal encoder della lista
     oe.fit(np.array(tag_tot).reshape(-1, 1))
-    t = oe.transform( np.array(tag_tot).reshape(-1, 1))
-    
+    t = oe.transform(np.array(tag_tot).reshape(-1, 1))
+
     oe.fit(np.array(df['produttore']).reshape(-1, 1))
     p = oe.transform(np.array(df['produttore']).reshape(-1, 1))
 
     def diz_nome_codice(keys, values):
         diz = {k: int(v) for k, v in zip(keys, values)}
         return diz
-    
-    
+
     diz_prod = diz_nome_codice(df['produttore'], p)
-    
+
     new_prod = [diz_prod[p] for p in df['produttore']]
-        
+
     from sklearn.preprocessing import MultiLabelBinarizer
 
     mlb = MultiLabelBinarizer()
 
-    df2 = pd.DataFrame(mlb.fit_transform(df.tags),columns=mlb.classes_, index=df.index)
-    
+    df2 = pd.DataFrame(mlb.fit_transform(df.tags),
+                       columns=mlb.classes_, index=df.index)
+
+    # from sklearn.preprocessing import MinMaxScaler
+
+    # scaler = MinMaxScaler(feature_range=(0, 1))
+    # df['prezzo'] = scaler.fit_transform(df['prezzo'].array.reshape(-1,1))
+
     df['produttore'] = new_prod
     df = pd.concat([df, df2], axis=1)
     del df['tags']
-    del df['_id']
     # del df['prezzo']
-    
+    df = df.dropna()
     return df
-        
+
 
 def predicta(df):
-    X = np.array(df.iloc[:, 1:])
-    knn = NearestNeighbors(n_neighbors=3, algorithm='auto').fit(X)
+    X = np.array(df.iloc[:, 2:])
+    knn = NearestNeighbors(n_neighbors=5, algorithm='auto').fit(X)
     distances, indices = knn.kneighbors(X)
     return distances, indices
 
-def prodotti_correlati(df, acquisto):
+
+def prodotti_correlati(df, _id_acquisto):
     _, indices = predicta(df)
-    indice_acquisto = df[df['nome'] == acquisto].index 
+    prodotto_acquistato = df.loc[df['_id'] == _id_acquisto]
+    print(prodotto_acquistato.index[0], type(prodotto_acquistato))
+
     for x in indices:
-        if x[0] == indice_acquisto:
-            ls_prodotti_correlati = [df.loc[y, 0] for y in x[1:]]
+        if x[0] == prodotto_acquistato.index[0]:
+            ls = [df.iloc[y, 0] for y in x[1:]]
 
-    return ls_prodotti_correlati
+    return ls
 
+def consiglia_prodotti(carrello):
+    '''funzione che consiglia altri prodotti
 
-
-# preprocessing dati di mongo
-df = preprocessing(Extract().format())
-
-print(df)
-# print (df.iloc[:, 1:])
-print(predicta(df))
+    :param ObjectID carrello: _ID DEL PRODOTTO
+    '''    
+    # preprocessing dati di mongo
+    df = preprocessing(Extract().format())
+    ls_prodotti_correlati = prodotti_correlati(df, carrello)
